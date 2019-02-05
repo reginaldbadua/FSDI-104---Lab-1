ac1475c6d4ab74e7ddb31a09e11362dc2cd10155
reginaldbadua <46497568+reginaldbadua@users.noreply.github.com>
2019-02-05 12:27:25 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+var score = [80, 72, 91];
+
+alert (score.length ); 
+
+for (let i = 0; i < score.length; i++) {
+
+  // function sum(a, b) {
+	// let x;
+	// x=a+b;
+  // return x;;
+}
+var average =
